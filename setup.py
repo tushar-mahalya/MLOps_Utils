4c8f622ba0eae35df880bc115d8a0879503bb38c
Tushar Sharma <tusharmahalya@gmail.com>
2024-03-27 19:01:27 +0530

    Chanaged name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_mlops_utils',
+    name='MLOps_Utils',
     version='0.1',
     packages=find_packages(),
     description='A package for managing Machine Learning Operations for my projects.',
